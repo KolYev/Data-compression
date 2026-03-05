1770882ba1285313c836c38851382348fc5db298
KolYev <evgeniy.kolesenko@gmail.com>
2026-03-05 21:50:36 +0300

    подсчёт каждого символа в строке

--- main.cpp
+++ main.cpp
@@ -1,18 +1,38 @@
 #include <iostream>
 using namespace std;
 
-
 int main()
 {
-    //создаём массив
-    char text[] = "Hello World";
-    //создаём указатель на нулевой элемент массива
+    // создаём массив
+    char text[] = "Hello World!!!!Hello World";
+    // создаём указатель на нулевой элемент массива
     char *ptr = text;
 
-    for (int i = 0; i < 5; i++)
+    // массив для подсчёта
+    int count[256] = {0};
+
+    cout << text << endl;
+
+    for (int i = 0; text[i] != '\0'; i++)
     {
-        cout<<ptr[i]<<endl;
+        unsigned char c = text[i];
+        count[c]++;
     }
-    
+
+    for (int i = 0; i < 256; i++)
+    {
+        if (count[i] > 0)
+        {
+            if (i >= 32 && i <= 126) // печатные символы
+            {
+                cout << char(i) << ": " << count[i] << endl;
+            }
+            else
+            { // управляющие символы
+                cout << "[" << i << "]: " << count[i] << endl;
+            }
+        }
+    }
+
     return 0;
 }
