9ee5ff1f77bf11574a9255e0465a3e9bfcff1996
KolYev <evgeniy.kolesenko@gmail.com>
2026-03-05 19:55:09 +0300

    создаём указатель на нулевой элемент массива

--- main.cpp
+++ main.cpp
@@ -1,22 +1,18 @@
 #include <iostream>
-#include <vector>
-#include <list>
-#include <map>
 using namespace std;
 
 
 int main()
 {
-    vector<int> x;
+    //создаём лист
+    int arr[5]{6, 4, 2, 9, 7};
+    //создаём указатель на нулевой элемент массива
+    int *ptr = arr;
 
-    x.push_back(72);
-    x.push_back(954);
-    x.push_back(3000);
-    x.push_back(123);
 
-    for (int i = 0; i < x.size(); i++)
+    for (int i = 0; i < 5; i++)
     {
-        cout<<x[i]<<endl;
+        cout<<ptr[i]<<endl;
     }
     
     return 0;
