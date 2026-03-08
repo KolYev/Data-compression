e914cabeffac580678af851617972b1b8188d1cd
KolYev <evgeniy.kolesenko@gmail.com>
2026-03-08 21:47:17 +0300

    рекурсивная функция построения дерева

--- main.cpp
+++ main.cpp
@@ -10,8 +10,9 @@ struct Node
 };
 
 // функция создания узла
-Node* createNode(int value, char ch) {
-    Node* newNode = new Node;
+Node *createNode(int value, char ch)
+{
+    Node *newNode = new Node;
     newNode->a = value;
     newNode->c = ch;
     newNode->left = nullptr;
@@ -19,8 +20,44 @@ Node* createNode(int value, char ch) {
     return newNode;
 }
 
+// рекурсивная функция построения дерева
+Node *Tree(int arr1[], char arr2[], int left, int right)
+{
+    if (left > right)
+    {
+        return nullptr;
+    }
+
+    int mid = (left + right) / 2;
+    Node *node = createNode(arr1[mid], arr2[mid]);
+    node->left = Tree(arr1, arr2, left, mid - 1);
+    node->right = Tree(arr1, arr2, mid + 1, right);
+
+    return node;
+}
+
+// void printTreeDetailed(Node *root, std::string prefix = "", bool isLeft = true)
+// {
+//     if (root == nullptr)
+//     {
+//         std::cout << prefix << "+- null" << std::endl;
+//         return;
+//     }
+
+//     std::cout << prefix;
+//     std::cout << (isLeft ? "+-- " : "+-- ");
+
+//     std::cout << "[" << root->a << ", " << root->c << "]" << std::endl;
+
+//     std::string newPrefix = prefix + (isLeft ? "|  " : "    ");
+
+//     printTreeDetailed(root->left, newPrefix, true);
+//     printTreeDetailed(root->right, newPrefix, false);
+// }
+
 // функция, удаляющая элементы массива, заполненные нулями
-int zero_zeros(int arr[], int size) {
+int zero_zeros(int arr[], int size)
+{
     int new_size = 0;
 
     for (int i = 0; i < size; i++)
@@ -29,7 +66,7 @@ int zero_zeros(int arr[], int size) {
         {
             arr[new_size] = arr[i];
             new_size++;
-        } 
+        }
     }
 
     size = new_size;
@@ -44,20 +81,17 @@ void arrays_sort(int arr1[], char arr2[], int size)
     {
         for (int j = 0; j < size - i - 1; j++)
         {
-            if (arr1[j] < arr1[j+1])
+            if (arr1[j] < arr1[j + 1])
             {
-                arr1[j] = arr1[j] + arr1[j+1];
-                arr2[j] = arr2[j] + arr2[j+1];
-                arr1[j+1] = arr1[j] - arr1[j+1];
-                arr2[j+1] = arr2[j] - arr2[j+1];
-                arr1[j] = arr1[j] - arr1[j+1];
-                arr2[j] = arr2[j] - arr2[j+1];
+                arr1[j] = arr1[j] + arr1[j + 1];
+                arr2[j] = arr2[j] + arr2[j + 1];
+                arr1[j + 1] = arr1[j] - arr1[j + 1];
+                arr2[j + 1] = arr2[j] - arr2[j + 1];
+                arr1[j] = arr1[j] - arr1[j + 1];
+                arr2[j] = arr2[j] - arr2[j + 1];
             }
-            
         }
-        
     }
-    
 }
 
 int main()
@@ -78,7 +112,7 @@ int main()
 
     int size = zero_zeros(count, 256);
     int *char_count = new int[size]; // массив, который хранит количество элементов каждого символа
-    char *char_el = new char[size]; // массив, который хранит каждый символ, который присутствовал в тексте
+    char *char_el = new char[size];  // массив, который хранит каждый символ, который присутствовал в тексте
     int j = 0;
 
     for (int i = 0; i < 256; i++)
@@ -96,13 +130,17 @@ int main()
             { // управляющие символы
                 cout << "[" << i << "]: " << count[i] << endl;
             }
-        } else {
-
+        }
+        else
+        {
         }
     }
 
     arrays_sort(char_count, char_el, size);
-    
+
+    Node *root = Tree(char_count, char_el, 0, size - 1);
+    // std::cout << "Tree Structure:" << std::endl;
+    // printTreeDetailed(root);
 
     delete[] char_count;
     delete[] char_el;
